45b4657b810ebca84208b8cf1de70e17d3941e1b
Cleiton C. Salvagni <81267618+cleitoncsalvagni@users.noreply.github.com>
2021-08-25 22:08:26 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+package exercicio;
+
+/* public class Main {
+
+	public static void main(String[] args) {
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		int gastosTrimestre;
+		int mediaMensal;
+		boolean gastoControlado = true;
+		
+		gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+		mediaMensal = gastosTrimestre / 3;
+		System.out.println("Os gastos do trimestre foram: " + gastosTrimestre);
+		System.out.println("A média de gastos mensais foi: " + mediaMensal);
+		
+		if(mediaMensal < 30000) {
+			System.out.println("O valor esta controlado.");
+			gastoControlado = true; 
+		} else {
+			System.out.println("O valor não esta controlado!");
+			gastoControlado = false; 			
+		}
+		
+				
+	}
+
+}  
+
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		float imc = 0;
+		float altura;
+		float peso;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite sua altura: ");
+		altura = scanner.nextFloat();
+		
+		System.out.print("Digite seu peso: ");
+		peso = scanner.nextFloat();
+		
+		imc = peso / (altura * altura);
+		
+		System.out.println("Seu IMC é: " + imc); 
+		
+		if(imc < 18.5) {
+			System.out.print("O seu IMC indica que voce esta muito magro, deve utilizar uma dieta com um indice calorico um pouco mais elevado, utilizando carboidratos e proteinas!!!");
+		} else if(imc >= 18.5 && imc < 24.9) {
+			System.out.print("O seu IMC indica que voce esta no nivel ideal, então mantenha este equilibrio na alimentação!");
+		} else if(imc >= 24.9 && imc < 29.9) {
+			System.out.print("O seu IMC indica que voce esta no nivel de sobrepeso, entao o indicado e que reduza a quantidade de carboidratos ingeridos diariamente!");
+		} else if(imc >= 29.9 && imc < 39.9) {
+			System.out.print("O seu IMC indica que voce esta em um nivel de obesidade grau 2, entao voce deve reduzir drasticamente o seu consumo calorico diario, utilizando uma dieta de deficit calorico e tambem deve praticar exercicios fisicos!");
+		} else {
+			System.out.print("O seu IMC indica que voce esta a beira da morte, ingira apenas frutas e agua e pratique exercicios!");
+		}
+	}
+} 
+
+public class Main {
+	public static void main(String[] args) {
+		
+		for(int i= 120; i<= 280; i++) {
+			System.out.println (i);
+		} 
+				
+	}
+} */
+	
